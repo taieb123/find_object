3477e09aec272fadea0bf5772e7997b7152117a9
achrafmak <maktoufachraf@gmail.com>
2019-06-12 23:56:01 +0100

    added register page

--- public/js/app.js
+++ public/js/app.js
@@ -66530,6 +66530,23 @@ module.exports = function(module) {
     });
   }
 
+  function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        $('#imagePreview').css('background-image', 'url(' + e.target.result + ')');
+        $('#imagePreview').hide();
+        $('#imagePreview').fadeIn(650);
+      };
+
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+
+  $("#imageUpload").change(function () {
+    readURL(this);
+  });
   Stepper();
   initMap();
   initMapDetail();
