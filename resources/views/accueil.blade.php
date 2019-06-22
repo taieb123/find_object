10f7fcc59a23078019c14abde6ee35e7232abe38
achrafmak <maktoufachraf@gmail.com>
2019-06-22 17:58:57 +0100

    enhance main page

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -4,9 +4,8 @@
 <header class="masthead">
     <div class="container">
         <div class="intro-text">
-            <div class="intro-lead-in">Welcome To Our Studio!</div>
-            <div class="intro-heading text-uppercase">It's Nice To Meet You</div>
-            <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a>
+            <div class="intro-heading text-uppercase">RETROUVEZ VOS OBJETS PERDUS !</div>
+            <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#portfolio">Tell Me More</a>
         </div>
     </div>
 </header>
--- resources/views/layouts/body.blade.php
+++ resources/views/layouts/body.blade.php
@@ -10,7 +10,46 @@
 <body id="page-top">
   @include('index.navbar') 
   @yield('content')
-    <p>typo</p> 
+  <section class="bg-light page-section" id="portfolio">
+      <div class="container">
+          <div class="row">
+              <div class="col-lg-12 text-center">
+                  <h2 class="section-heading text-uppercase">Portfolio</h2>
+                  <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+              </div>
+          </div>
+          <div class="row df-ais">
+              <div class="col-sm-6 portfolio-item">
+                  <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+                      <div class="portfolio-hover">
+                          <div class="portfolio-hover-content">
+                              <i class="fa fa-plus fa-3x"></i>
+                          </div>
+                      </div>
+                      <img class="img-fluid" src="../images/lost.jpg" alt="">
+                  </a>
+                  <div class="portfolio-caption">
+                      <h4>Lost Items</h4>
+                      <p class="text-muted">Find your lost item here</p>
+                  </div>
+              </div>
+              <div class="col-sm-6 portfolio-item">
+                  <a class="portfolio-link" href="{{ url('search') }}">
+                      <div class="portfolio-hover">
+                          <div class="portfolio-hover-content">
+                              <i class="fa fa-plus fa-3x"></i>
+                          </div>
+                      </div>
+                      <img class="img-fluid" src="../images/search.jpg" alt="">
+                  </a>
+                  <div class="portfolio-caption">
+                      <h4>Found Items</h4>
+                      <p class="text-muted">Help other get their lost items back</p>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </section>
     @include('index.footer')
     @include('index.script')
 </body>
